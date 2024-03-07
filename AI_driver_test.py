9b898fcff60bf4ce6765e396d91848b30329e6ba
abdo amr <abdu.amr.fatouh@gmail.com>
2024-03-07 06:16:28 +0300

    code tester of the model

--- AI_driver_test.py
+++ AI_driver_test.py
@@ -2,18 +2,52 @@ import inferenc
 from inferenc import inference_img, cv2, np
 from picamera2 import Picamera2
 import os
+import shutil
+import re
 import atexit
-takedir = ""
+from gamepad import XboxController,ser
+import time
+
+vid_dir = './vids/'
+joy = XboxController()
+isPaused = False
+fps = 15
+def motors_switch(state:bool):
+    msg = bytearray([ord('M'),state])
+    ser.write(msg)
+def led_switch(state:bool):
+    msg = bytearray([ord('L'),state])
+    ser.write(msg)
+def resetArduion():
+    ser.write(b'R0')
+def getState():
+    state = ''
+    if joy.LeftDPad:
+        state = 'left'
+        # return state
+    elif joy.RightDPad:
+        state = 'right'
+        # return state
+    else:
+        state = 'idle'
+        # return state
+    return state
+def natural_sort_key(s, _nsre=re.compile('([0-9]+)')):
+    return [
+        int(text)
+        if text.isdigit() else text.lower()
+        for text in _nsre.split(s)]
+
 def creat_take_dir(directory):
     count = 0
     for root, dirs, files in os.walk(directory):
         # print(dirs)
-        for dir in dirs:
-            if 'take' in dir:
+        for file in files:
+            if 'take' in file:
                 count += 1
-    dir_name = directory+f"/take{count}/"
-    os.makedirs(dir_name)
-    return dir_name
+    vidname = directory+f"/take{count}.mp4"
+    # os.makedirs(dir_name)
+    return vidname
 
 def convert_images_to_video(image_folder, video_name, fps):
     images = [img for img in os.listdir(image_folder) if img.endswith(".png") or img.endswith(".jpg")]
@@ -24,17 +58,20 @@ def convert_images_to_video(image_folder, video_name, fps):
     height, width, layers = frame.shape
 
     video = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc(*'mp4v'), fps, (width,height))
-
-    for image in images:
+    sorted_images = sorted(images, key=natural_sort_key)
+    for image in sorted_images:
         video.write(cv2.imread(os.path.join(image_folder, image)))
-        # os.remove(os.path.join(image_folder, image))
+        os.remove(os.path.join(image_folder, image))
 
     cv2.destroyAllWindows()
     video.release()
 
 def exit_handler():
     print("Terminat!")
-    convert_images_to_video(takedir,takedir+'vid.mp4',30)
+    resetArduion()
+    picam2.close()
+    convert_images_to_video(vid_dir+'/Tmp',vid_name,fps)
+    # shutil.rmtree(vid_dir+'/Tmp')
 
 atexit.register(exit_handler)
 
@@ -45,17 +82,46 @@ picam2 = Picamera2()
 picam2.configure(picam2.create_preview_configuration(main={"format":"RGB888","size": (600, 600)}))
 picam2.start()
 i = 0
-takedir = creat_take_dir('./vidStream')
+vid_name = creat_take_dir(vid_dir)
+try:
+    os.makedirs(vid_dir+'/Tmp')
+except:
+    pass
+
+print("Press START to start record!")
+while(not joy.Start): pass
+led_switch(1)
+motors_switch(1)
+print("Go!")
+time.sleep(0.5)
+print("Press Y to pause dataRecording")
+
 while True:
-    im = picam2.capture_array()
-    out = inference_img(im)
-    id = np.argmax(out)
-    pred = labels[id]
-    print(pred)
-    cv2.putText(im,f"Prediction: {pred}",org = (5,5),fontFace= cv2.FONT_HERSHEY_SIMPLEX,fontScale = 1 , color=(0,0,255))
-    img_name = str(i)
-    img_name = img_name.zfill(len(img_name)+1)
-    cv2.imwrite(takedir+f"/{img_name}.jpg",im)
-    i += 1
+    if joy.Start:
+        break
+    if joy.Y:
+        if not isPaused:
+            print("pause!")
+            motors_switch(0)
+            led_switch(0)
+            isPaused = True
+        else:
+            print("Start Recording!")
+            led_switch(1)
+            motors_switch(1)
+            isPaused = False
+        while(joy.Y): pass
+    if not isPaused: 
+        im = picam2.capture_array()
+        out = inference_img(im)
+        id = np.argmax(out)
+        pred = labels[id]
+        real = getState()
+        print(pred)
+        cv2.putText(im,f"Prediction: {pred}",org = (10,30),fontFace= cv2.FONT_HERSHEY_SIMPLEX,fontScale = 1 , color=(0,0,255),thickness=2)
+        cv2.putText(im,f"real: {real}",org = (10,60),fontFace= cv2.FONT_HERSHEY_SIMPLEX,fontScale = 1 , color=(0,0,255),thickness=2)
+        img_name = str(i)
+        cv2.imwrite(vid_dir+f"/Tmp/{img_name}.jpg",im)
+        i += 1
     # cv2.imshow("Camera", im)
     cv2.waitKey(1)
